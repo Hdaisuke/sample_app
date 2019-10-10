9a0371846c8c2bc66f570a7ac78660d8476039ed
Hdaisuke <testmane30@gmail.com>
2019-10-10 15:35:03 +0000

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user          #ユーザーのブラウザ内の一時cookiesに暗号化済みのユーザーIDが自動で作成される？
+      redirect_to user      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else                    #userはuser_url(user.id）を指している
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
